27416017b4304688c3c5d51ad3c265a990f77fa8
DuratulAin <146700506+DuratulAin@users.noreply.github.com>
2023-11-09 15:27:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import json
 
 # Function to load a model from a pickle file
 def load_model(model_file):
@@ -11,25 +12,29 @@ def load_model(model_file):
 # Streamlit UI elements
 st.title('Model Prediction App')
 
-# Load sample data from a CSV file
-sample_data = pd.read_csv('Raw data all.csv')
+# Load the JSON data
+with open('Raw spectral 1.json') as jsonfile:
+    data = json.load(jsonfile)
+
+# Convert the JSON data to a pandas DataFrame
+df = pd.DataFrame(data)
 
 # Print out the raw spectral CSV where the first row contains wavelength and the second row contains spectral value
-# st.write('Raw spectral CSV:')
-# st.table(sample_data)
+#st.write('Raw spectral CSV:')
+#st.table(sample_data)
 
 # Load the UMAP model from the joblib file
-umap_model = load_model('umap_model_10.joblib').transform(sample_data)
+umap_model = load_model('umap_model_10.joblib').transform(df)
 
 # Button to trigger prediction for both models
 if st.button('Predict'):
     # Load the Linear Regression model and make a prediction
     linear_reg_model = load_model('linear_reg_model_10.joblib')
-    linear_reg_prediction = linear_reg_model.predict(sample_data)
+    linear_reg_prediction = linear_reg_model.predict(df)
 
     # Load the Decision Tree model and make a prediction
     decision_tree_model = load_model('decision_tree_model_10.joblib')
-    decision_tree_prediction = decision_tree_model.predict(sample_data)
+    decision_tree_prediction = decision_tree_model.predict(df)
 
     # Load the Linear Regression model with UMAP and make prediction
     linear_reg_model_umap = load_model('linear_reg_model_umap_10.joblib')
@@ -43,13 +48,13 @@ if st.button('Predict'):
     st.markdown('<font size="6"><b>Predictions:</b></font>', unsafe_allow_html=True)
 
     st.markdown('**Linear Regression Model:**')
-    st.markdown(f'<font size="5"><b>{linear_reg_prediction[2]} g/dL</b></font>', unsafe_allow_html=True)
+    st.markdown(f'<font size="5"><b>{linear_reg_prediction[0]} g/dL</b></font>', unsafe_allow_html=True)
 
     st.markdown('**Decision Tree Model:**')
-    st.markdown(f'<font size="5"><b>{decision_tree_prediction[2]} g/dL</b></font>', unsafe_allow_html=True)
+    st.markdown(f'<font size="5"><b>{decision_tree_prediction[0]} g/dL</b></font>', unsafe_allow_html=True)
 
     st.markdown('**Linear Regression Model with UMAP:**')
-    st.markdown(f'<font size="5"><b>{linear_reg_umap_pred[2]:.1f} g/dL</b></font>', unsafe_allow_html=True)
+    st.markdown(f'<font size="5"><b>{linear_reg_umap_pred[0]:.1f} g/dL</b></font>', unsafe_allow_html=True)
 
     st.markdown('**Decision Tree Model with UMAP:**')
-    st.markdown(f'<font size="5"><b>{decision_tree_umap_pred[2]} g/dL</b></font>', unsafe_allow_html=True)
+    st.markdown(f'<font size="5"><b>{decision_tree_umap_pred[0]} g/dL</b></font>', unsafe_allow_html=True)
