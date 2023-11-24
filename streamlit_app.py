3624a8e06bb2e6f8ce2c63fa12160d7f019301ca
DuratulAin <146700506+DuratulAin@users.noreply.github.com>
2023-11-24 15:15:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def load_model(model_file):
 st.title('Model Prediction App')
 
 # Load the JSON data
-with open('Raw data all.csv') as jsonfile:
+with open('Raw data all.json') as jsonfile:
  data = json.load(jsonfile)
 
 # Convert the JSON data to a pandas DataFrame
