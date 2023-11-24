b4d4d64b57bf4ba16b8d8570c5f96d8a5e7e4f94
DuratulAin <146700506+DuratulAin@users.noreply.github.com>
2023-11-24 15:47:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,63 @@ import requests
 import base64
 from io import StringIO
 import csv
- 
+
+# Function to retrieve data from Xano and save it as a CSV file
+def retrieve_data():
+    xano_api_endpoint = 'https://x8ki-letl-twmt.n7.xano.io/api:U4wk_Gn6/spectral_data'
+
+    response = requests.get(xano_api_endpoint)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Convert the Xano data to a pandas DataFrame
+        df = pd.DataFrame(data)
+
+        # Save the data as a CSV file
+        df.to_csv('retrieved_data.csv', index=False)
+
+        return df
+    else:
+        st.error("Failed to retrieve data. Status code:", response.status_code)
+        return None
+
+# Main Streamlit app
+def main():
+    st.title("Streamlit App")
+
+    # Retrieve data from Xano
+    data_df = retrieve_data()
+
+    # Display the retrieved data
+    if data_df is not None:
+        st.write("Retrieved Data:")
+        st.table(data_df)
+
+        # Button to display and download the CSV file
+        if st.button('Download CSV File'):
+            st.markdown("### Downloading CSV File...")
+
+            # Create a link to download the CSV file
+            st.markdown(get_binary_file_downloader_html('retrieved_data.csv', 'CSV File'), unsafe_allow_html=True)
+
+# Function to create a download link for a file
+def get_binary_file_downloader_html(file_path, file_label='File'):
+    with open(file_path, 'r') as file:
+        data = file.read()
+    b64 = base64.b64encode(data.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{file_path}">{file_label}</a>'
+    return href
+
+# Function to load a model from a pickle file
+def load_model(model_file):
+    with open(model_file, 'rb') as f:
+        model = joblib.load(f)
+    return model
+
+if __name__ == "__main__":
+    main()
+
 # # Enable CORS
 # Server.enableCORS = True
 
