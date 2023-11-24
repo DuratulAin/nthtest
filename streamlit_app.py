f1d68003c2ee7a8876c9976d9382bb0497e34f39
DuratulAin <146700506+DuratulAin@users.noreply.github.com>
2023-11-24 16:24:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def load_model(model_file):
 st.title('Model Prediction App')
 
 # Load the CSV data from Xano
-xano_data_df = pd.read_csv('Raw spectral 2.csv')
+xano_data_df = pd.read_csv('Dummy 1.csv')
 
 # Load the UMAP model from the joblib file
 umap_model = load_model('umap_model_10.joblib').transform(xano_data_df)
