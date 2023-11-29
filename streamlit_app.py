af8170e09f6d7fe5e4ab2a5e65ea1cf565086d50
DuratulAin <146700506+DuratulAin@users.noreply.github.com>
2023-11-29 10:28:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,41 +25,11 @@ def retrieve_data():
         st.error("Failed to retrieve data. Status code:", response.status_code)
         return None
 
-# Main Streamlit app
-def main():
-    st.title("Streamlit App")
-
-    # Retrieve data from Xano
-    data_df = retrieve_data()
-
-    # Display the retrieved data
-    if data_df is not None:
-        st.write("Retrieved Data:")
-        st.table(data_df)
-
-        # Button to display and download the CSV file
-        if st.button('Download CSV File'):
-            st.markdown("### Downloading CSV File...")
-
-            # Create a link to download the CSV file
-            st.markdown(get_binary_file_downloader_html('retrieved_data.csv', 'CSV File'), unsafe_allow_html=True)
-
-# Function to create a download link for a file
-def get_binary_file_downloader_html(file_path, file_label='File'):
-    with open(file_path, 'r') as file:
-        data = file.read()
-    b64 = base64.b64encode(data.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="{file_path}">{file_label}</a>'
-    return href
-
 # Function to load a model from a pickle file
 def load_model(model_file):
     with open(model_file, 'rb') as f:
         model = joblib.load(f)
     return model
-
-if __name__ == "__main__":
-    main()
 
 # Streamlit UI elements
 st.title('Model Prediction App')
