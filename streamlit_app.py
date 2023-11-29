7e651141def2abf3080cb1290ee5a14877b896e2
DuratulAin <146700506+DuratulAin@users.noreply.github.com>
2023-11-29 10:25:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from io import StringIO
 
 # Function to retrieve data from Xano and save it as a CSV file
 def retrieve_data():
-    xano_api_endpoint = 'https://x8ki-letl-twmt.n7.xano.io/api:U4wk_Gn6/spectral_data/25'
+    xano_api_endpoint = 'https://x8ki-letl-twmt.n7.xano.io/api:U4wk_Gn6/spectral_data'
 
     response = requests.get(xano_api_endpoint)
 
@@ -18,12 +18,39 @@ def retrieve_data():
         df = pd.DataFrame(data)
 
         # Save the data as a CSV file
-        df.to_csv('xano_data.csv', index=False)
+        df.to_csv('retrieved_data.csv', index=False)
 
         return df
     else:
         st.error("Failed to retrieve data. Status code:", response.status_code)
         return None
+
+# Main Streamlit app
+def main():
+    st.title("Streamlit App")
+
+    # Retrieve data from Xano
+    data_df = retrieve_data()
+
+    # Display the retrieved data
+    if data_df is not None:
+        st.write("Retrieved Data:")
+        st.table(data_df)
+
+        # Button to display and download the CSV file
+        if st.button('Download CSV File'):
+            st.markdown("### Downloading CSV File...")
+
+            # Create a link to download the CSV file
+            st.markdown(get_binary_file_downloader_html('retrieved_data.csv', 'CSV File'), unsafe_allow_html=True)
+
+# Function to create a download link for a file
+def get_binary_file_downloader_html(file_path, file_label='File'):
+    with open(file_path, 'r') as file:
+        data = file.read()
+    b64 = base64.b64encode(data.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{file_path}">{file_label}</a>'
+    return href
 
 # Function to load a model from a pickle file
 def load_model(model_file):
@@ -31,35 +58,27 @@ def load_model(model_file):
         model = joblib.load(f)
     return model
 
+if __name__ == "__main__":
+    main()
+
 # Streamlit UI elements
 st.title('Model Prediction App')
 
 # Load the CSV data from Xano
-xano_data_df = pd.read_csv('xano_data.csv')
-
-# Load the original data
-original_data = pd.read_csv('Raw data all w.csv')
-
-# Combbine both Xano and original data in one csv file
-combined_data = pd.concat([xano_data_df, original_data])
-
-st.title('Xano')
-st.write(xano_data_df)
-st.title('Combined')
-st.write(combined_data)
+xano_data_df = pd.read_csv('retrieved_data.csv')
 
 # Load the UMAP model from the joblib file
-umap_model = load_model('umap_model_10.joblib').fit_transform(combined_data)
+umap_model = load_model('umap_model_10.joblib').transform(xano_data_df)
 
 # Button to trigger prediction for both models
 if st.button('Predict'):
     # Load the Linear Regression model and make a prediction
     linear_reg_model = load_model('linear_reg_model_10.joblib')
-    linear_reg_prediction = linear_reg_model.predict(combined_data)
+    linear_reg_prediction = linear_reg_model.predict(xano_data_df)
 
     # Load the Decision Tree model and make a prediction
     decision_tree_model = load_model('decision_tree_model_10.joblib')
-    decision_tree_prediction = decision_tree_model.predict(combined_data)
+    decision_tree_prediction = decision_tree_model.predict(xano_data_df)
 
     # Load the Linear Regression model with UMAP and make prediction
     linear_reg_model_umap = load_model('linear_reg_model_umap_10.joblib')
