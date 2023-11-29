5a3e2ac7bc12bb9f14dd9af92aee869dc443f6a9
DuratulAin <146700506+DuratulAin@users.noreply.github.com>
2023-11-29 11:55:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,8 @@ original_data = pd.read_csv('Raw data all w.csv')
 
 # Combine both datas
 combined_data = pd.concat([xano_data_df, original_data])
+st.write(xano_data_df)
+st.write(original_data)
 st.write(combined_data)
 
 # Load the UMAP model from the joblib file
